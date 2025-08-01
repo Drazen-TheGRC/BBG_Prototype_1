d232073dde7edcea36b05a8324995e0707b5869c
Drazen Drinic <drazendrinic@hotmail.com>
2025-08-01 22:36:39 +0200

    Add: player input for movement

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player; // Reference to the player GameObject
-    public Vector3 offset = new Vector3(0, 5, -7); // Offset from the player's position
+    private Vector3 offset = new Vector3(0, 5, -7); // Offset from the player's position
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20.0f; // Speed of the vehicle
+    public float TurnSpeed = 50.0f; // Speed of rotation
+    public float horizontalInput; // Input for horizontal movement
+    public float verticalInput; // Input for vertical movement
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +15,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal"); // Get horizontal input (A/D keys or Left/Right arrows)
+        verticalInput = Input.GetAxis("Vertical"); // Get vertical input (W/S keys or Up/Down arrows)
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // Rotate the vehicle based on horizontal input
+        transform.Rotate(Vector3.up * Time.deltaTime * TurnSpeed * verticalInput * horizontalInput  ); // Adjust rotation speed as needed
     }
 }
