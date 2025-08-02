51c0d146464162f50bf190d649a1cd3a059e465a
Drazen Drinic <drazendrinic@hotmail.com>
2025-08-02 19:27:12 +0200

    Add: camera toggle feature to PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : MonoBehaviour
     private float TurnSpeed = 50.0f; // Speed of rotation
     private float horizontalInput; // Input for horizontal movement
     private float verticalInput; // Input for vertical movement
+
+    // Cameras
+    public Camera mainCamera; // Reference to the main camera
+    public Camera hoodCamera; // Reference to the secondary camera
+    private KeyCode toggleCameraKey = KeyCode.F; // Key to toggle cameras
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,5 +27,12 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
         // Rotate the vehicle based on horizontal input
         transform.Rotate(Vector3.up * Time.deltaTime * TurnSpeed * verticalInput * horizontalInput  ); // Adjust rotation speed as needed
+
+        // Toggle camera view when the toggleCameraKey is pressed
+        if (Input.GetKeyDown(toggleCameraKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled; 
+        }
     }
 }
